178801606af5c0ad54a220dcad473262489fe92c
whitewolfzzxx <mboyandsboy@gmail.com>
2017-04-08 12:16:00 -0400

    Test ball program

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -3,25 +3,66 @@
 
 #include "stdafx.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "TicTacToe!");
 	//==================INITILIZE=====================//
-
-
+	sf::Vector2i screenSize(800, 600);
+	sf::RenderWindow window(sf::VideoMode(screenSize.x,screenSize.y), "TicTacToe!");
+	sf::Texture ballTexture;
+	sf::Sprite ballSprite;
+	sf::Vector2f ballVelocity;
+	if (!ballTexture.loadFromFile("Textures/Ball.png"))
+	{
+		std::cout << "Loading ball texture failed" << std::endl;
+	}
+	ballSprite.setTexture(ballTexture);
 	//==================GAME UPDATE=====================//
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+        while (window.pollEvent(event)) { if (event.type == sf::Event::Closed) window.close(); }
+		//Update stuff here
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			ballVelocity.x += 0.001;
+			//ballSprite.move(sf::Vector2f(0.1, 0));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			ballVelocity.x -= 0.001;
+			//ballSprite.move(sf::Vector2f(-0.1, 0));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			ballVelocity.y += 0.001;
+			//ballSprite.move(sf::Vector2f(0, 0.1));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			ballVelocity.y -= 0.001;
+			//ballSprite.move(sf::Vector2f(0, -0.1));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			ballVelocity = sf::Vector2f(0,0);
+
+		if (ballSprite.getPosition().x < 0)
+			ballVelocity = sf::Vector2f(-ballVelocity.x, ballVelocity.y);
+		if (ballSprite.getPosition().x > screenSize.x)
+			ballVelocity = sf::Vector2f(-ballVelocity.x, ballVelocity.y);
+		if (ballSprite.getPosition().y < 0)
+			ballVelocity = sf::Vector2f(ballVelocity.x, -ballVelocity.y);
+		if (ballSprite.getPosition().y > screenSize.y)
+			ballVelocity = sf::Vector2f(ballVelocity.x, -ballVelocity.y);
+		ballSprite.move(ballVelocity);
+
+	//==================GAME DRAW=====================//
         window.clear();
-        //window.draw(shape);
+		//Draw stuff here
+		window.draw(ballSprite);
+       
         window.display();
     }
 
